f0fc4cb4b96c921e2f69a0697c27bde241a50048
selinozluk <selinozluk36@gmail.com>
2025-07-21 23:24:55 +0300

    Add project files.

---
+++ TryCatch/Program.cs
@@ -0,0 +1,18 @@
+﻿bool isValidInput = false;
+
+while (!isValidInput) // Döngü yanlışsa dönecek
+{
+    try
+    {
+        Console.Write("Lütfen sayı giriniz: ");
+        string input = Console.ReadLine();
+        int number = Convert.ToInt32(input);
+
+        Console.WriteLine($"Girdiğiniz sayının karesi: {number * number}");
+        isValidInput = true;
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Hatalı veri girdiniz. Lütfen bir sayı girin.");
+    }
+}
